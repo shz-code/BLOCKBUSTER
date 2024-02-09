e58c7514a7b222bb0111f873edb326c88d4a0c8c
shz-code <web.shahidul.alam@gmail.com>
2024-02-09 18:29:29 +0600

    modify: wallet router

--- routes/walletRouter.js
+++ routes/walletRouter.js
@@ -4,13 +4,15 @@ const { Wallet } = require("../models/wallet");
 const handleGet = async (req, res) => {
     try {
         const walletId = req.params.wallet_id; 
-        const wallet = await Wallet.findOne({ wallet_id: walletId });
+        const wallet = await Wallet.findOne({ wallet_id: walletId }).select({wallet_id:1,balance:1,wallet_user:1,_id:0});
         
         if (!wallet) {
-            return res.status(404).json({ message: 'Wallet not found' });
+            return res.status(404).json({
+                message: `wallet with id: ${walletId} was not found`
+            });
         }
 
-        return res.json({ wallet });
+        return res.send(wallet);
     } catch (error) {
         console.error('Error fetching wallet:', error);
         return res.status(500).json({ message: 'Internal Server Error' });
