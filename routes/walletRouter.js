4690362684c35152d855ec90ed04a699657014b5
Tanvir Ahmed Khan <mostlytanvir@gmail.com>
2024-02-09 18:53:55 +0600

    Add api wallet balance update

--- routes/walletRouter.js
+++ routes/walletRouter.js
@@ -7,7 +7,7 @@ const handleGet = async (req, res) => {
         const wallet = await Wallet.findOne({ wallet_id: walletId });
         
         if (!wallet) {
-            return res.status(404).json({ message: 'Wallet not found' });
+            return res.status(404).json({ message: `wallet with id: ${walletId} was not found`});
         }
 
         return res.json({ wallet });
@@ -16,6 +16,38 @@ const handleGet = async (req, res) => {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 };
+const handleUpdateWalletBalance = async (req, res) => {
+    try {
+        const walletId = req.params.wallet_id; 
+        const newBalance = req.body.recharge; 
 
-router.route("/:wallet_id").get(handleGet);
+        if(newBalance<100 || newBalance>10000){
+            return res.status(400).json({ message: `invalid amount: ${newBalance}`});
+        }
+        
+        const updatedWallet = await Wallet.findOneAndUpdate(
+            { wallet_id: walletId },
+            { $inc: { balance: newBalance } },
+            { new: true } 
+        );
+
+
+
+        
+        if (!updatedWallet) {
+            return res.status(404).json({ message: `wallet with id: ${walletId} was not found`});
+        }
+
+        return res.json({
+            "wallet_id": walletId,
+            "wallet_balance": updatedWallet.balance,
+            "wallet_user": updatedWallet.wallet_user
+        });
+    } catch (error) {
+        console.error('Error fetching wallet:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+router.route("/:wallet_id").get(handleGet).put(handleUpdateWalletBalance);
 module.exports = router;
