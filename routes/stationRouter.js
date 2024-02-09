c30d061fe3fc6b29f6fc8a39615672ec1f113a43
shz-code <web.shahidul.alam@gmail.com>
2024-02-09 17:13:18 +0600

    add: /api/stations get

--- routes/stationRouter.js
+++ routes/stationRouter.js
@@ -2,7 +2,10 @@ const router = require("express").Router();
 const {Station} = require("../models/station")
 
 const handleGet = async (req,res) => {
-
+    const stations = await Station.find().select({station_id:1,station_name:1,longitude:1,latitude:1,_id:0});
+    return res.send({
+        stations: stations
+    })
 }
 
 const handlePost = async (req,res) => {
