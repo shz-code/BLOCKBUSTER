787f6245710fc9b3f5d568840a5f657c11ad5df2
shz-code <web.shahidul.alam@gmail.com>
2024-02-09 16:36:01 +0600

    add: /api/users post

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,19 @@
 const router = require("express").Router();
-const User = require("../models/user")
+const { User } = require("../models/user");
 
-const handleGet = async (req,res) => {
+const handleGet = async (req, res) => {};
 
-}
+const handlePost = async (req, res) => {
+  const body = req.body;
+  const user = new User(body);
+  try {
+    user.save();
+    return res.status(201).send(body);
+  } catch (err) {
+    return res.status(4004).send("Bad Request");
+  }
+};
 
-router.route("/").get(handleGet).post();
+router.route("/").get(handleGet).post(handlePost);
 
 module.exports = router;
