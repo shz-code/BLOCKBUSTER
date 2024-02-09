de1beb2134145b6fd3c0f4b566e58e14b0fdc0ab
shz-code <web.shahidul.alam@gmail.com>
2024-02-09 17:09:27 +0600

    add: /api/trains post

--- models/train.js
+++ models/train.js
@@ -1,13 +1,24 @@
 const { Schema, model } = require("mongoose");
 
 const trainSchema = Schema(
-  { 
+  {
     train_id: Number,
-    train_name: String ,
+    train_name: String,
     capacity: Number,
+    stops: [
+      {
+        station_id: {
+          type: Number,
+          ref: "Station"
+        },
+        arrival_time: String,
+        departure_time: String,
+        fare: Number,
+      }
+    ]
   },
   { timestamps: true }
 );
 
-module.exports.User = model("Train", trainSchema);
+module.exports.Train = model("Train", trainSchema);
 
--- routes/trainRouter.js
+++ routes/trainRouter.js
@@ -1,10 +1,29 @@
 const router = require("express").Router();
-const Train = require("../models/train")
+const { Train } = require("../models/train");
 
-const handleGet = async (req,res) => {
+const handleGet = async (req, res) => {};
 
-}
+const handlePost = async (req, res) => {
+  body = req.body;
+  const train = new Train(body);
 
-router.route("/").get(handleGet).post();
+  const stops = body.stops;
+
+  try {
+    // station.save();
+    return res.status(201).send({
+      train_id: body.train_id,
+      train_name: body.train_name,
+      capacity: body.capacity,
+      service_start: stops[0].departure_time,
+      service_ends: stops[stops.length - 1].arrival_time,
+      num_stations: stops.length,
+    });
+  } catch (err) {
+    return res.status(404).send("Bad Request");
+  }
+};
+
+router.route("/").get(handleGet).post(handlePost);
 
 module.exports = router;
